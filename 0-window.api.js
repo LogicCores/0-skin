4c027144b36bbe473ca33efdbe91528a60a2701a
Christoph Dorn <christoph@christophdorn.com>
2015-11-23 14:12:14 -0800

    window api

---
+++ 0-window.api.js
@@ -0,0 +1,29 @@
+
+exports.forLib = function (LIB) {
+    
+    var exports = {};
+
+    exports.adapters = {
+    }
+
+    exports.forContexts = function (contexts) {
+    
+        var exports = {};
+        
+        var Context = exports.Context = function (defaults) {
+            var self = this;
+        
+            var state = {
+            };
+            LIB._.merge(state, LIB._.cloneDeep(defaults));
+
+            self.config = defaults;
+        }
+        Context.prototype = Object.create(LIB.EventEmitter.prototype);
+        Context.prototype.contexts = contexts;
+    
+        return exports;
+    }
+
+    return exports;
+}
